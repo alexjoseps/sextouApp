2c4d0880966ec1217025cd7a63f2f5691805dc6d
= <=>
2023-11-28 20:19:00 -0300

    initial commit

---
+++ app/src/main/java/com/example/sextouapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.sextouapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.search_item -> {
+                true
+            }
+            R.id.category_item -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
