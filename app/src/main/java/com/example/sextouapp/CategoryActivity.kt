8ab9ad93ea5aa6d944982e3b06d772ef9420a81e
Alex Souza <alexjoseps@outlook.com>
2023-12-01 08:57:11 -0300

    feat: toolbar/actionbar and category activity

---
+++ app/src/main/java/com/example/sextouapp/CategoryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sextouapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CategoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category)
+    }
+}
--- app/src/main/java/com/example/sextouapp/MainActivity.kt
+++ app/src/main/java/com/example/sextouapp/MainActivity.kt
@@ -2,19 +2,37 @@ package com.example.sextouapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(true);
+        supportActionBar?.setTitle("Sextou!");
+        supportActionBar?.setSubtitle("Ache seu rolê")
+
+        val listView : ListView = findViewById(R.id.events_list)
+        val minhaLista = ArrayList<String>()
+        minhaLista.add("Balburdia");
+        minhaLista.add("Trezze");
+        minhaLista.add("Don Pub");
+        val adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1, minhaLista)
+        listView.adapter = adapter
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater: MenuInflater = menuInflater
-        inflater.inflate(R.menu.main, menu)
+        inflater.inflate(R.menu.main_menu, menu)
         return true
     }
 
@@ -23,7 +41,12 @@ class MainActivity : AppCompatActivity() {
             R.id.search_item -> {
                 true
             }
-            R.id.category_item -> {
+            R.id.events_item -> {
+                Log.i(null, "Eventos");
+                true
+            }
+            R.id.categories_item -> {
+                Log.i(null, "Categorias");
                 true
             }
             else -> super.onOptionsItemSelected(item)
